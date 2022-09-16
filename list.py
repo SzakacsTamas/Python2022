2cdc06e7902b32b870bb5ce9467d445b24923471
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2022-09-16 08:45:22 +0200

    asdw

---
+++ list.py
@@ -0,0 +1,12 @@
+lista=["alma", "banán", "cigány", 42]
+print (lista)
+print (lista[0])
+print (len(lista))
+print (lista[len(lista) -1])
+print (type(lista[   len(lista) -1]))
+lista[2]="cukornád"
+print(lista)
+lista.append("fgh")
+print(lista)
+lista.insert(1,"igen")
+print(lista)
