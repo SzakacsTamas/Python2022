f7e29c8ca8e4bd106d1b5fa1a4946e28dda0cc29
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2022-11-07 13:20:31 +0100

    dsgth

--- ASD.py
+++ ASD.py
@@ -1,5 +1,16 @@
 import matplotlib.pyplot as plt
 
+def fv(a,b,c):
+    xPont=[]
+    yPont=[]
+
+    for x in range(-10, 10):
+        xPont.append(x)
+        yPont.append(a*x**2 + b*x +c)
+
+    return [xPont, yPont]
+    
+
 x=[1,10]
 y=[10,1]
 
@@ -25,6 +36,9 @@ for x in range(-10, 10):
     yPont.append(16*x**2 - 3 +16)
 
 plt.plot(xPont,yPont)
+
+pontok=fv(1,1,1)
+plt.plot(pontok)[0], pontok 1
 
     
 plt.show()
