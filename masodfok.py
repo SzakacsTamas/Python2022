290ad7ccb0e752b59aed119d08e7b8e4c6b2fafa
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2022-11-10 13:19:50 +0100

    fdgr

--- masodfok.py
+++ masodfok.py
@@ -2,6 +2,24 @@ import math
 
 #a*X2+b*x+c
 
+def egyenlet (a,b,c):
+    szoveg="0 = "
+    if a !=0:
+        szoveg+=str(a)+"x²"
+
+    if b >0:
+        szoveg+=" + " + str(b) + " x "
+    elif b<0:
+        szoveg+=" " + str(b) + " x "
+    if c >0:
+        szoveg+=" + " + str(b)
+    elif c<0:
+        szoveg+=" " + str(b)
+
+    return szoveg
+    
+        
+
 a = int(input("a="))
 b = int(input("b="))
 c = int(input("c="))
@@ -18,7 +36,8 @@ elif diszkriminans == 0:
 else:
     x1=(-b+math.sqrt(diszkriminans)) / (2*a)
     x2=(-b+math.sqrt(diszkriminans)) / (2*a)
-
-#gyok=math.sqrt(1)
+    print("2 megoldás: x1:{}, x2:{}".format(x1,x2))
+    
+#gyok=math.sqrt(1)00
 #print(gyok)
-
+print(egyenlet(a,b,c))
