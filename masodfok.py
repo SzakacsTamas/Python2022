404fe6cd579b581895ee83d317e12c4bb34f7ccd
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2022-11-17 13:18:21 +0100

    4dfbvc

--- masodfok.py
+++ masodfok.py
@@ -27,11 +27,18 @@ c = int(input("c="))
 #(-b+-gyök(b2-4ac)) / 2a
 
 #print(math.sqrt(3))
+
+x1=""
+x2=""
+
 diszkriminans=b*b-4*a*c
 if diszkriminans < 0:
     print("Nincs megoldás")
 elif diszkriminans == 0:
     megoldas=-b / (2*a)
+    x1=megoldas
+    x2=megoldas
+    
     print("1 megoldas: {}".format(megoldas))
 else:
     x1=(-b+math.sqrt(diszkriminans)) / (2*a)
@@ -41,3 +48,11 @@ else:
 #gyok=math.sqrt(1)00
 #print(gyok)
 print(egyenlet(a,b,c))
+
+#a*(x-x1)*(x-x2)=0
+print(a)
+print(x1)
+print(x2)
+
+
+
