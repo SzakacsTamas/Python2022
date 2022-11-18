5c8450f54a550f150be935489b0320cf34404587
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2022-11-18 13:20:08 +0100

    fefsefg

---
+++ szamBe.py
@@ -0,0 +1,11 @@
+#Számbekérő modul
+#Többféle paraméterezéssel
+#2022/11/18 Szakács Tamás
+
+def szamBe(szoveg,tort=False, minimum=False, maximum=False):
+    print(szoveg)
+    print(tort)
+szamBe("Aggyá meg szamot: ", )
+
+print("asdasda",)
+print("3243423")
