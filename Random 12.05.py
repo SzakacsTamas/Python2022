f1f2f12eb68c9a80ab1281c15aea66d13e490925
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2022-12-05 13:19:44 +0100

    fkdgfc

---
+++ Random 12.05.py
@@ -0,0 +1,16 @@
+import random
+import math
+l=[]
+for i in range(10):
+    szam=random.random()
+    l.append(math.floor (szam*90)+10)
+    
+print(i)
+
+
+l=[]
+for i in range(10):
+    
+    l.append(random.randint10,99))
+    
+print(i)
--- Szerelem Gép _.py
+++ Szerelem Gép _.py
@@ -3,15 +3,29 @@
 #Max=int(input("Mennyi legyen a max?: "))
 #for i in range(loop):
 import random
-
+szam=(random.randint(0,101))
+fiuk=["Olcsi", "Huni", "Tomi", "Csocsi", "Bence", "LaciMaci", "Konyhi", "Magas", "BMW", "Vak", "Petyko", "Ady", "Martinez(Budos)", "Ferko", "Marci", "Levike"]
+Lányok=["Vivike", "Hanna(Nagy)", "Hanna(ViviWhite)", "Jázmin", "Andi", "Hédi","J1","J2"]
+#print(random.randrange(3, 9))
 #    print(random.randint(Min, Max))
-
+def kuki2():
+    for i in range(100):
+        boys=(random.choice(fiuk))
+        girls=(random.choice(Lányok))
+    print("A fiú:", boys)
+    print("A Lány:", girls)
+    print(szam, "%", "Illetek össze")
+    asd=int(input("Akarsz még egyet?(igen=0):" ))
+    if asd == 0:
+        kuki2()
+kuki2()
+        
 
 def kuki():
     for i in range(100):
         kivagy=str(input("Ki vagy te?: "))
         ki=str(input("Ki legyen a Párod?: "))
-        szam=(random.randint(0,101))
+        
         szazalek=100
         if kivagy=="Tomi" and ki=="Hédi":
             print(szazalek, "%", "Illetek Össze!")
@@ -21,5 +35,5 @@ def kuki():
             print(szam, "%", "Illetek össze !:D")
         
 
-kuki()
+#kuki()
 
