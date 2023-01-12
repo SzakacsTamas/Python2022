6e666e7f5136a022c4e5689e50e9313f995963fd
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-01-12 13:18:34 +0100

    asekfae

--- filekezeles.py
+++ filekezeles.py
@@ -2,6 +2,30 @@
 f=open("proba.txt","w")
 
 f.write("Szia")
-f.write("world")
+f.write("\n")
+f.write("Lányok")
 
 f.close()
+
+f=open("proba.txt","r")
+
+szoveg=(f.read())
+
+sorok=szoveg.split("\n")
+print(sorok)
+
+f.close()
+
+f=open("proba.txt","r")
+
+sorok=[]
+for sor in f:
+    sorok.append(sor.replace("\n","").replace("\r",""))
+
+print(sorok)
+
+
+f.close()
+
+
+
