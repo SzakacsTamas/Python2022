aa833aa6f7ecaffb97cf59417efc501a54753754
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-02-01 14:02:05 +0100

    fdksfgsdgdfhdfhdfkhdfhjdfh

--- Érettségi feladatok/Szamok/szamok.py
+++ Érettségi feladatok/Szamok/szamok.py
@@ -13,3 +13,21 @@ for sor in f:
 f.close()
 print("2. feladat")
 print("Az adatfile-ban " + str(len(kerdesek)) + " kérdés van.")
+
+matek=[]
+for e in kerdesek:
+    if e[3] == "matematika":
+        matek.append(e[2])
+
+
+print("Az adatfajlban " + str(len(matek)) +
+      " matematika feladat van, 1 pontot er " + str(matek.count(1)) + " feladat, 2 pontot er " + str(matek.count(2)) +
+      " feladat, 3 pontot er " + str(matek.count(3)) + " feladat.")
+
+
+valaszok=[]
+for e in kerdesek:
+    valaszok.append(e[1])
+
+valaszok=[e[1] for e in kerdesek]
+print("A válaszok szám értéke {}-től {}-ig tart.".format(min(valaszok),max(valaszok)))
