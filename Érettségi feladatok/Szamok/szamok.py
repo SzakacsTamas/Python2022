75cbcfc2264fcb5254eafad6bf5af16954eda439
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-02-02 12:34:37 +0100

    jglhjgfébvc

--- Érettségi feladatok/Szamok/szamok.py
+++ Érettségi feladatok/Szamok/szamok.py
@@ -1,3 +1,4 @@
+import random
 f=open("felszam.txt","r")
 
 
@@ -31,3 +32,34 @@ for e in kerdesek:
 
 valaszok=[e[1] for e in kerdesek]
 print("A válaszok szám értéke {}-től {}-ig tart.".format(min(valaszok),max(valaszok)))
+
+temakorok=[]
+for e in kerdesek:
+    if e[3] not in temakorok:
+        temakorok.append(e[3])
+
+print()
+print("5.Feladat")  #    I  
+#                        V   EZZEL A STRING-el FŰZD ÖSSZE A LISTA ELEMEIT
+print("A témakörök: " + ", ".join(temakorok))
+    
+print("6. Feladat")
+
+
+beker=input("Milyen Témakörből szeretne kérdést kapni? ")
+ujLista=[e for e in kerdesek if e[3]== beker]
+print(ujLista)
+
+sorsolt=random.choice(ujLista)
+valasz=input(sorsolt[0])
+if int(valasz)== sorsolt[1]:
+    print(str(sorsolt[2]) + " pont")
+else:
+    print("0 pont"  + str(sorsolt[1]) + "pont")
+
+
+
+
+
+
+
