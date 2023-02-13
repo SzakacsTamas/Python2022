c0e66055936615132f589ef9048dce4dc941f6d0
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-02-13 12:34:50 +0100

    sdasddcx

--- Érettségi feladatok/TesztVerseny/tesztverseny.py
+++ Érettségi feladatok/TesztVerseny/tesztverseny.py
@@ -1,3 +1,21 @@
+def pontSzamit(valasz,helyes):
+    pont=0
+    for sorszam,betu in enumerate(valasz):
+        if betu == helyes[sorszam]:
+            if sorszam < 5:
+                pont+=3
+            elif sorszam < 10:
+                pont += 4
+            elif sorszam < 14:
+                pont += 5
+            else:
+                pont+=6
+
+    return pont 
+        
+ 
+        
+
 f=open("valaszok.txt")
 
 adatok=f.read().split("\n")
@@ -59,11 +77,16 @@ for e in valaszok:
         db+=1
 
 
-print("A feladatra {0} fő, a versenyzők {1}-a adott helyes választ.".format(db,db/len(valaszok)))
+print("A feladatra {0} fő, a versenyzők {1:.2%}-a adott helyes választ.".format(db,db/len(valaszok)))
     
 
+f=open("pontok.txt","w")
+for e in valaszok:
+    pont=pontSzamit(e[1],helyes)
+    
+    f.write(e[0] + " " + str(pont)+ "\n")
 
-
+f.close()
 
 
     
