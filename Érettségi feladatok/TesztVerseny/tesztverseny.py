77a2f331db5f13ef83433c4ed2305a2c9961ac83
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-02-13 13:18:36 +0100

    dfcxvxc

--- Érettségi feladatok/TesztVerseny/tesztverseny.py
+++ Érettségi feladatok/TesztVerseny/tesztverseny.py
@@ -81,15 +81,25 @@ print("A feladatra {0} fő, a versenyzők {1:.2%}-a adott helyes választ.".form
     
 
 f=open("pontok.txt","w")
+
+eredmenyek=[]
 for e in valaszok:
     pont=pontSzamit(e[1],helyes)
     
     f.write(e[0] + " " + str(pont)+ "\n")
+    eredmenyek.append([pont, e[0]])
 
 f.close()
 
+#eredmenyek.sort()
+#eredmenyek.reverse()
+#print(eredmenyek[:10])
 
-    
+csakPontok=set()
+for e in eredmenyek:
+    csakPontok.add(e[0])
+
+print(csakPontok)[-3:]
     
 
 
