d14843ae857618e143cf8f4e12d91db85c714f7e
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-02-17 09:38:28 +0100

    nnnnbbbb

--- Ágazati Alapvizsga 2022_1/Feladat 2.0.py
+++ Ágazati Alapvizsga 2022_1/Feladat 2.0.py
@@ -1,3 +1,23 @@
-import haromszog
+import Feladat2
 
-haromszog.haromszog
+def ujabb(darab=3):
+    haromszogek=[]
+    for i in range(darab):
+        haromszogek.append(Feladat2.haromszog())
+
+    for e in haromszogek:
+        print("\ta={}, b={}, c={}".format(e[0],e[1],e[2]))
+    return haromszogek
+
+def haromszogE(haromszogek):
+    for e in haromszogek:
+        if sum(e)-max(e) > max(e):
+            print("Lehet Háromszög!")
+
+        else:
+            print("Nem lehet Háromszög!")
+
+h=ujabb(3)
+haromszogE(h)
+#lista=Feladat2.haromszog()
+#print(lista)
