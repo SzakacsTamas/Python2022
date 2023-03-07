57bd52591d346a933a0a1fe750373c07f38103a4
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-03-07 11:30:07 +0100

    fgdfcvc

--- Alapok Gyakorlása.py
+++ Alapok Gyakorlása.py
@@ -6,10 +6,58 @@ print("Ennyi gyümölcs van: {}".format(len(gyumolcsok)))
 index=gyumolcsok.index("barac")
 gyumolcsok[index]+="k"
 print(gyumolcsok)
-
+rovid=[]
 print("rövid gyümölcsök")
 for i in range(len(gyumolcsok)):
     if len(gyumolcsok[i]) < 5:
-        print(gyumolcsok[i])
-#for i in gyumolcsok:
-#    print(i)
+        rovid.append(gyumolcsok[i])
+print(rovid)
+
+rovid=[]
+
+for e in gyumolcsok:
+    if len(e) < 5:
+        rovid.append(e)
+
+print(rovid)
+
+rovid=[e for e in gyumolcsok if len(e) < 5]
+print(rovid)
+
+rovid=[]
+
+i=0
+while i< len(gyumolcsok):
+    if len(gyumolcsok[i]) < 5:
+        rovid.append(gyumolcsok[i])
+    i+=1
+
+print(rovid)
+
+rovid=[]
+i=0
+while True:
+    print(i)
+    if len(gyumolcsok[i]) < 5:
+        rovid.append(gyumolcsok[i])
+    i+=1
+    if len(gyumolcsok)-1 == i:
+        break
+    
+    
+print(gyumolcsok[:2])
+
+print(gyumolcsok[-2:])
+
+print(gyumolcsok[len(gyumolcsok)-2:])
+
+print(gyumolcsok[1:-1])
+
+paratlan=gyumolcsok[1::2]
+print(paratlan)
+paros=gyumolcsok[0::2]
+print(paros)
+
+masolat=gyumolcsok
+masolat.reverse()
+print(" /-/ ".join(gyumolcsok[::-1]))
